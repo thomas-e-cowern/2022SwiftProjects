1174cf2686ce0c9e34c1bd6848df6ce604ef5604
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-29 09:24:57 -0400

    alteliers farsiani initial commit

---
+++ ateliers_farsiani/ateliers_farsiani/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ateliers_farsiani
+//
+//  Created by Thomas Cowern New on 3/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ateliers_farsiani/ateliers_farsiani/ateliers_farsianiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ateliers_farsianiApp.swift
+//  ateliers_farsiani
+//
+//  Created by Thomas Cowern New on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct ateliers_farsianiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
