80cd03428fe1dbaeb02328fa780b805fe100bdb0
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-29 14:51:13 -0400

    added video card view

---
+++ ateliers_farsiani/ateliers_farsiani/VideoCardView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoCardView.swift
+//  ateliers_farsiani
+//
+//  Created by Thomas Cowern New on 3/29/22.
+//
+
+import SwiftUI
+
+struct VideoCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VideoCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoCardView()
+    }
+}
