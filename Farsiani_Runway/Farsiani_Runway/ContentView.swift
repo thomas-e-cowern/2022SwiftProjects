ac05aaf09beeaab78bd553f6f71ed0f870b5732a
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-22 09:18:45 -0400

    farsiani runway initial commit

---
+++ Farsiani_Runway/Farsiani_Runway/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Farsiani_Runway
+//
+//  Created by Thomas Cowern New on 4/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Farsiani_Runway/Farsiani_Runway/Farsiani_RunwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Farsiani_RunwayApp.swift
+//  Farsiani_Runway
+//
+//  Created by Thomas Cowern New on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Farsiani_RunwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
