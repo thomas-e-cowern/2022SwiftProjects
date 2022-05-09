3800eafb69fb8d22aa908d0ad55b38d0e7a1c4e2
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-09 12:24:26 -0400

    pity party initail commit

---
+++ PityParty/PityParty/PityPartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PityPartyApp.swift
+//  PityParty
+//
+//  Created by Thomas Cowern New on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct PityPartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
