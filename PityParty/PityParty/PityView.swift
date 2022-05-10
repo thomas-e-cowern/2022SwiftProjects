a453ab3c3f91200acc9437944314253fafc899c7
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-10 06:59:18 -0400

    added pity view

---
+++ PityParty/PityParty/PityView.swift
@@ -0,0 +1,20 @@
+//
+//  PityView.swift
+//  PityParty
+//
+//  Created by Thomas Cowern New on 5/10/22.
+//
+
+import SwiftUI
+
+struct PityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PityView_Previews: PreviewProvider {
+    static var previews: some View {
+        PityView()
+    }
+}
