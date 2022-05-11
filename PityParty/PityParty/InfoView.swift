ab2ddae2c179048794c8fb48577f7effa27e52f4
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-11 08:38:20 -0400

    modified navbar in info view

--- PityParty/PityParty/InfoView.swift
+++ PityParty/PityParty/InfoView.swift
@@ -9,26 +9,29 @@ import SwiftUI
 
 struct InfoView: View {
     var body: some View {
-        
-        VStack {
-            Image("world")
-                .resizable()
-                .frame(maxWidth: .infinity, maxHeight: 200)
+        ScrollView {
+            VStack {
+                Image("world")
+                    .resizable()
+                    .frame(maxWidth: .infinity, maxHeight: 200)
+                    .padding()
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            
+            VStack (alignment: .leading) {
+                
+                Text("How to use Pity Party:")
+                    .padding(.bottom, 10)
+                    .font(.largeTitle)
+                
+                Text("Pity Party is designed for those moments when you're stuck, frustrated or feeling sorry for yourself and need a minute to get yourself together.  Pressing the begin button starts a 60 second countdown with a random background color of dark shades.  Use this minute to work through your feelings and accept them.  At the end of the minute, the colors will brighten and some motivational messages will appear.  That means it's time to put your self pity aside and get into a better mindset to meet the challanges that remain.")
+                
+                Text("You can do this, you can handle what comes!")
+                    .padding(.top, 10)
+                    .font(.title)
+            }
+            .padding()
         }
-        
-        VStack (alignment: .leading) {
-            
-            Text("How to use Pity Party:")
-                .padding(.bottom, 10)
-                .font(.largeTitle)
-            
-            Text("Pity Party is designed for those moments when you're stuck, frustrated or feeling sorry for yourself and need a minute to get yourself together.  Pressing the begin button starts a 60 second countdown with a random background color of dark shades.  Use this minute to work through your feelings and accept them.  At the end of the minute, the colors will brighten and some motivational messages will appear.  That means it's time to put your self pity aside and get into a better mindset to meet the challanges that remain.")
-            
-            Text("You can do this, you can handle what comes!")
-                .padding(.top, 10)
-                .font(.title)
-        }
-        .padding()
     }
 }
 
