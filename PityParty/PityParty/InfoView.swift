c0574a05b98d479bc2d328275f5ea6969659468f
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-11 08:11:05 -0400

    added explaination tex, padding, and alignment to text views in info view

--- PityParty/PityParty/InfoView.swift
+++ PityParty/PityParty/InfoView.swift
@@ -9,10 +9,13 @@ import SwiftUI
 
 struct InfoView: View {
     var body: some View {
-        VStack {
+        VStack (alignment: .leading) {
             Text("How to use Pity Party:")
-            Text("Info View")
+                .padding()
+                .background(Color.blue)
+            Text("Pity Party is designed for those moments when your stuck, frustrated or feeling sorry for yourself and need a minute to get yourself together.  Pressing the begin button starts a 60 second countdown with a random background color of dark shades.  Use this minute to work through your feelings and accept them.  At the end of the minute, the colors will brighten and some motivational messages will appear.  That means it's time to put your self pity aside and get into a better mindset to meet the challanges that remain.  You can do this, you can handle what comes!")
         }
+        .padding()
     }
 }
 
