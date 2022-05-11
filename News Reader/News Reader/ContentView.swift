4783375f326402f1323205bc4024e82c3d0552e1
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-11 14:00:12 -0400

    news reader initial commit

---
+++ News Reader/News Reader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  News Reader
+//
+//  Created by Thomas Cowern New on 5/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ News Reader/News Reader/News_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_ReaderApp.swift
+//  News Reader
+//
+//  Created by Thomas Cowern New on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct News_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
