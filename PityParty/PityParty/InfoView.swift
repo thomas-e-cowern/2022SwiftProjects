8d089218a1294190825e347408cdbda1aa43f634
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-11 08:21:08 -0400

    added world picture to project assets

--- PityParty/PityParty/InfoView.swift
+++ PityParty/PityParty/InfoView.swift
@@ -11,9 +11,14 @@ struct InfoView: View {
     var body: some View {
         VStack (alignment: .leading) {
             Text("How to use Pity Party:")
-                .padding()
-                .background(Color.blue)
-            Text("Pity Party is designed for those moments when your stuck, frustrated or feeling sorry for yourself and need a minute to get yourself together.  Pressing the begin button starts a 60 second countdown with a random background color of dark shades.  Use this minute to work through your feelings and accept them.  At the end of the minute, the colors will brighten and some motivational messages will appear.  That means it's time to put your self pity aside and get into a better mindset to meet the challanges that remain.  You can do this, you can handle what comes!")
+                .padding(.bottom, 10)
+                .font(.largeTitle)
+            
+            Text("Pity Party is designed for those moments when you're stuck, frustrated or feeling sorry for yourself and need a minute to get yourself together.  Pressing the begin button starts a 60 second countdown with a random background color of dark shades.  Use this minute to work through your feelings and accept them.  At the end of the minute, the colors will brighten and some motivational messages will appear.  That means it's time to put your self pity aside and get into a better mindset to meet the challanges that remain.")
+            
+            Text("You can do this, you can handle what comes!")
+                .padding(.top, 10)
+                .font(.title)
         }
         .padding()
     }
