f2170b169991689161678f8ea13421a005fe7e74
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-12 17:10:26 -0400

    updated padding to image in story detail view

--- News Reader/News Reader/StoryDetailView.swift
+++ News Reader/News Reader/StoryDetailView.swift
@@ -12,56 +12,68 @@ struct StoryDetailView: View {
     @State var article: Article
     
     var body: some View {
-        VStack {
-            VStack (alignment: .center) {
-                
-                if article.urlToImage != nil {
-                    AsyncImage(url: URL(string: article.urlToImage!)) { phase in
-                        switch phase {
-                        case .empty:
-                            ProgressView()
-                        case .success(let image):
-                            image.resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(maxWidth: .infinity, maxHeight: 150)
-                        case .failure:
-                            Image("newspaper")
-                        @unknown default:
-                            EmptyView()
-                        }
-                    }
-                } else {
-                    Image("newspaper")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(maxWidth: 400, maxHeight: 200)
-                }
-            
-                Form {
-                    
-                    Section (header: Text("Headline:")) {
-                        Text(article.title)
+        VStack (alignment: .center) {
+            if article.urlToImage != nil {
+                AsyncImage(url: URL(string: article.urlToImage!)) { phase in
+                    switch phase {
+                    case .empty:
+                        ProgressView()
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(maxWidth: .infinity, maxHeight: 150)
+                            .padding(.leading, 40)
+                            .padding(.trailing, 40)
+                            .padding(.top, 40)
+                            .padding(.bottom, 40)
+                            .background(Color.white)
+                    case .failure(let fail):
+                        Text("Failure: \(fail.localizedDescription)")
+                        Image("newspaper")
+                            .resizable()
                             .padding()
-                            .font(.headline)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxWidth: 400, maxHeight: 200)
+                    @unknown default:
+                        EmptyView()
+                    }
+                }
+            } else {
+                Text("Else")
+                Image("newspaper")
+                    .resizable()
+                    .padding()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: 400, maxHeight: 200)
+            }
+
+            Form {
+                
+                Section (header: Text("Headline:")) {
+                    Text(article.title)
+                        .padding()
+                        .font(.headline)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .multilineTextAlignment(.leading)
+                }
+                
+                Section (header: Text("Summary:")) {
+                    if article.description != nil {
+                        Text(article.description!)
+                            .padding()
+                            .font(.body)
                             .fixedSize(horizontal: false, vertical: true)
                             .multilineTextAlignment(.leading)
+                    } else {
+                        Text("There is no summary for this article")
                     }
-                    
-                    Section (header: Text("Summary:")) {
-                        if article.description != nil {
-                            Text(article.description!)
-                                .padding()
-                                .font(.body)
-                                .fixedSize(horizontal: false, vertical: true)
-                                .multilineTextAlignment(.leading)
-                        }
-                    }
-                    
-                    Section (header: Text("Story Link:")) {
-                        if article.url != nil {
-                            Link(destination: URL(string: article.url!)!) {
-                                Text("See the full story here...")
-                            }
+                }
+                
+                Section (header: Text("Story Link:")) {
+                    if article.url != nil {
+                        Link(destination: URL(string: article.url!)!) {
+                            Text("See the full story here...")
                         }
                     }
                 }
