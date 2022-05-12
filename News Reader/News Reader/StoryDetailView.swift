fb79e14acf50ad7cc6bf278833ac97c5cc02bc17
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-12 14:03:19 -0400

    updated story detal view with modifiers

--- News Reader/News Reader/StoryDetailView.swift
+++ News Reader/News Reader/StoryDetailView.swift
@@ -13,59 +13,45 @@ struct StoryDetailView: View {
     
     var body: some View {
         VStack (alignment: .center) {
-            
-//            if article.urlToImage != nil {
-//                AsyncImage(url: URL(string: article.urlToImage!)) { image in
-//                    image.resizable()
-//                    .aspectRatio(contentMode: .fit)
-//                    .frame(maxWidth: 300, maxHeight: 150)
-//                } placeholder: {
-//                    ProgressView()
-//                }
-//            }
-            
             VStack {
                 if article.urlToImage != nil {
                     AsyncImage(url: URL(string: article.urlToImage!)) { phase in
-                    switch phase {
-                    case .empty:
-                        ProgressView()
-                    case .success(let image):
-                        image.resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(maxWidth: 300, maxHeight: 150)
-                    case .failure:
-                        Image("newspaper")
-                    @unknown default:
-                        EmptyView()
-                    }
+                        switch phase {
+                        case .empty:
+                            ProgressView()
+                        case .success(let image):
+                            image.resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(maxWidth: 300, maxHeight: 150)
+                        case .failure:
+                            Image("newspaper")
+                        @unknown default:
+                            EmptyView()
+                        }
                     }
                 } else {
                     Image("newspaper")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(maxWidth: 300, maxHeight: 150)
+                        .frame(maxWidth: 400, maxHeight: 200)
                 }
-            }
-            
-            Text(article.title)
-                .padding()
-                .font(.body)
-                .fixedSize(horizontal: false, vertical: true)
-                .multilineTextAlignment(.leading)
-            
-            if article.description != nil {
-                Text(article.description!)
+                
+                Text(article.title)
                     .padding()
-                    .font(.body)
+                    .font(.headline)
                     .fixedSize(horizontal: false, vertical: true)
                     .multilineTextAlignment(.leading)
+                
+                if article.description != nil {
+                    Text(article.description!)
+                        .padding()
+                        .font(.body)
+//                        .frame(maxWidth: 300)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .multilineTextAlignment(.leading)
+                }
             }
         }
-    }
-    
-    func getArticleImage (url: String) {
-        
     }
 }
 
