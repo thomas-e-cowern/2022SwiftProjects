879df9ca6a0a004bcd412e5bd734a10fbe13dcc8
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-12 07:20:24 -0400

    added article model to project

---
+++ News Reader/News Reader/ArticleModel.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleModel.swift
+//  News Reader
+//
+//  Created by Thomas Cowern New on 5/12/22.
+//
+
+import Foundation
+
+class Article {
+    var title = ""
+    var urlToImage = ""
+    var url = ""
+    var description = ""
+    var category = ""
+}
