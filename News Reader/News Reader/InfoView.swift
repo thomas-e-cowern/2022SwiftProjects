56d571a2a5770c0c1b515c070d6a726858c137c3
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-13 07:59:38 -0400

    added info view to project

---
+++ News Reader/News Reader/InfoView.swift
@@ -0,0 +1,20 @@
+//
+//  InfoView.swift
+//  News Reader
+//
+//  Created by Thomas Cowern New on 5/13/22.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
