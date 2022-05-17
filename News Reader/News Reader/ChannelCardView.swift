6a41509f43ec6cee85823495d77c7c675a578470
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-17 11:05:20 -0400

    added channel card view

---
+++ News Reader/News Reader/ChannelCardView.swift
@@ -0,0 +1,20 @@
+//
+//  ChannelCardView.swift
+//  News Reader
+//
+//  Created by Thomas Cowern New on 5/17/22.
+//
+
+import SwiftUI
+
+struct ChannelCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChannelCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChannelCardView()
+    }
+}
