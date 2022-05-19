4754a3a22fb646ff91d09b940b02a1241d639249
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-19 09:01:11 -0400

    added header view

---
+++ DayOneClone/DayOneClone/HeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  HeaderView.swift
+//  DayOneClone
+//
+//  Created by Thomas Cowern New on 5/19/22.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text("Header View")
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView()
+    }
+}
