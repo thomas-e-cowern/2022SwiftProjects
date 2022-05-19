143394e99a0574de48d5ff7222560384f3a323fe
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-19 07:50:02 -0400

    added list view to project

---
+++ DayOneClone/DayOneClone/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  DayOneClone
+//
+//  Created by Thomas Cowern New on 5/19/22.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
