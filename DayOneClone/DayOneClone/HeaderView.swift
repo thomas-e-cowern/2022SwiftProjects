7c43f4c8b206072855c6dd693d50c8c011912bd9
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-19 09:36:05 -0400

    added buttons and formatting to header view

--- DayOneClone/DayOneClone/HeaderView.swift
+++ DayOneClone/DayOneClone/HeaderView.swift
@@ -9,10 +9,39 @@ import SwiftUI
 
 struct HeaderView: View {
     var body: some View {
-        VStack {
-            Text("Header View")
+        GeometryReader { metrics in
+            VStack {
+                HStack {
+                    Button {
+                        // Chose a photo
+                    } label: {
+                        Image(systemName: "camera")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(Color.white)
+                    }
+                    .padding()
+                    .frame(width: 70, height: 70)
+                
+                    Spacer()
+                        .frame(width: 50)
+                    
+                    Button {
+                        // New entry
+                    } label: {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(Color.white)
+                    }
+                    .padding()
+                    .frame(width: 70, height: 70)
+                }
+            }
+            .frame(height: metrics.size.height * 0.4)
+            .frame(maxWidth: .infinity)
+            .background(Color("Blue"))
         }
-        .background(Color("Blue"))
     }
 }
 
