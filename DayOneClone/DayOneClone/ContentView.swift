456c12ce802ef46eb568d4f48bd6a13ff7a4400b
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-19 07:32:57 -0400

    day one clone initial commit

---
+++ DayOneClone/DayOneClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DayOneClone
+//
+//  Created by Thomas Cowern New on 5/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DayOneClone/DayOneClone/DayOneCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayOneCloneApp.swift
+//  DayOneClone
+//
+//  Created by Thomas Cowern New on 5/19/22.
+//
+
+import SwiftUI
+
+@main
+struct DayOneCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
