91b22528a5ab75d7fd685642aa577f7c92bd50f2
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-19 11:16:44 -0400

    added date button and date picker to new entry view

--- DayOneClone/DayOneClone/Views/NewEntryView.swift
+++ DayOneClone/DayOneClone/Views/NewEntryView.swift
@@ -11,10 +11,32 @@ struct NewEntryView: View {
     
     @Environment(\.presentationMode) var presentationMode
     
+    @State private var description: String = ""
+    @State private var date = Date()
+    
     var body: some View {
         NavigationView {
             Form {
-                Text("New Entry View")
+                Section (header: Text("Description")) {
+                    TextEditor(text: $description)
+                        .multilineTextAlignment(.leading)
+                        .frame(height: 150)
+                }
+                
+                DatePicker(
+                    "Date",
+                    selection: $date,
+                    displayedComponents: [.date]
+                )
+                
+                Button {
+                    // Activate date picker
+                } label: {
+                    Text("Date")
+                }
+                .frame(maxWidth: .infinity)
+
+                
             } // End of form
             .navigationTitle("Add New Entry")
             .navigationBarTitleDisplayMode(.inline)
