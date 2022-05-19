e8e8f8166fa636ea10bc8bd4432a3ae301497b94
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-19 08:58:10 -0400

    added new entry view

---
+++ DayOneClone/DayOneClone/NewEntryView.swift
@@ -0,0 +1,20 @@
+//
+//  NewEntryView.swift
+//  DayOneClone
+//
+//  Created by Thomas Cowern New on 5/19/22.
+//
+
+import SwiftUI
+
+struct NewEntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewEntryView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewEntryView()
+    }
+}
