e6eb7e1ca066ded2421c6e28347996a557e1e551
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-20 05:47:06 -0400

    updated calendar and camero buttons in new entry view

--- DayOneClone/DayOneClone/Views/NewEntryView.swift
+++ DayOneClone/DayOneClone/Views/NewEntryView.swift
@@ -14,71 +14,70 @@ struct NewEntryView: View {
     @State private var description: String = ""
     @State private var date = Date()
     @State private var calendarId: Int = 0
+    @State private var isHidden: Bool = true
     
     var body: some View {
         NavigationView {
-//            Form {
-//                Section (header: Text("Description")) {
-            VStack {
+            VStack (alignment: .leading) {
                 TextEditor(text: $description)
-                            .multilineTextAlignment(.leading)
-                            .frame(height: 150)
-                            .navigationTitle("Add New Entry")
-                            .navigationBarTitleDisplayMode(.inline)
-                            .toolbar {
-                                ToolbarItem(placement: .navigationBarTrailing) {
-                                    Button {
-                                        presentationMode.wrappedValue.dismiss()
-                                    } label: {
-                                        Image(systemName: "x.circle")
-                                    }
-                                }
-                                
-                                ToolbarItem(placement: .navigationBarLeading) {
-                                    Button {
-                                        // Save
-                                    } label: {
-                                        Text("Save")
-                                    }
-                                }
+                    .padding(.horizontal)
+                    .multilineTextAlignment(.leading)
+                    .frame(height: 150)
+                    .border(Color.blue)
+                    .navigationTitle("Add New Entry")
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            Button {
+                                presentationMode.wrappedValue.dismiss()
+                            } label: {
+                                Image(systemName: "x.circle")
+                            }
                         }
-            } // End of toolbar
-//                }
+                        
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            Button {
+                                // Save
+                            } label: {
+                                Text("Save")
+                            }
+                        }
+                } // End of toolbar
                 
+                HStack {
+                    Button {
+                        isHidden = false
+                    } label: {
+                        Image(systemName: "calendar")
+                                .foregroundColor(.blue)
+                    }
+                    .frame(width: 70, height: 70)
 
-//                Section (header: Text("Add Image")) {
-//                    Button {
-//                        // Add imgage
-//                    } label: {
-//                        Image(systemName: "camera")
-//                    }
-//
-//                }
-                
-                
-//            } // End of form
-            
-            HStack {
-//                    Image(systemName: "calendar")
-//                        .foregroundColor(.blue)
-                DatePicker(
-                    "",
-                    selection: $date,
-                    displayedComponents: [.date]
-                )
-                .id(calendarId)
-                .onChange(of: date) { _ in
-                    calendarId += 1
+                    if !isHidden {
+                        DatePicker(
+                            "",
+                            selection: $date,
+                            displayedComponents: [.date]
+                        )
+                        .id(calendarId)
+                        .onChange(of: date) { _ in
+                            calendarId += 1
+                            isHidden = true
+                        }
+                    }
+                    
+                    Button {
+                        // Add imgage
+                    } label: {
+                        Image(systemName: "camera")
+                    }
+                    .frame(width: 70, height: 70)
+                    
+                    Spacer()
                 }
-                
-                Button {
-                    // Add imgage
-                } label: {
-                    Image(systemName: "camera")
-                }
-                
-                Spacer()
             }
+    
+           
 
         } // End of navigation view
     }
