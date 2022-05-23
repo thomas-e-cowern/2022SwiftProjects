c02a315b7b1fb54892cf2252b0b040d969fa7d7d
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-23 10:50:27 -0400

    added persistence controller

---
+++ DayOneClone/PersistenceController.swift
@@ -0,0 +1,49 @@
+//
+//  PersistenceController.swift
+//  DayOneClone
+//
+//  Created by Thomas Cowern New on 5/23/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct PersistenceController {
+    // A singleton for our entire app to use
+    static let shared = PersistenceController()
+
+    // Storage for Core Data
+    let container: NSPersistentContainer
+
+    // A test configuration for SwiftUI previews
+    static var preview: PersistenceController = {
+        let controller = PersistenceController(inMemory: true)
+
+        // Create 10 example programming languages.
+        for _ in 0..<10 {
+            let language = ProgrammingLanguage(context: controller.container.viewContext)
+            language.name = "Example Language 1"
+            language.creator = "A. Programmer"
+        }
+
+        return controller
+    }()
+
+    // An initializer to load Core Data, optionally able
+    // to use an in-memory store.
+    init(inMemory: Bool = false) {
+        // If you didn't name your model Main you'll need
+        // to change this name below.
+        container = NSPersistentContainer(name: "Main")
+
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                fatalError("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+}
